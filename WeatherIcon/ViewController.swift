f8d0f9bcf08621505e900bb859b78bcda84a0791
大石弘一郎 <k_ooishi@waku-2.com>
2017-01-27 15:01:59 +0900

    リファクタ

--- WeatherIcon/ViewController.swift
+++ WeatherIcon/ViewController.swift
@@ -18,42 +18,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let weatherAPI = WAPIManager(apiKey: "9eeaea80076ac9f4b5f19ec445d707f3", temperatureFormat: .Celsius, lang: .English)
-//
-//        weatherAPI.currentWeatherByCityNameAsJson(cityName: "London") {
-//            (WeatherResult) -> Void in
-//            print(WeatherResult)
-//        }
-        
+
         locationManager = CLLocationManager()
         locationManager.delegate = self
-        //位置情報取得の可否。バックグラウンドで実行中の場合にもアプリが位置情報を利用することを許可する
         locationManager.requestAlwaysAuthorization()
-        //位置情報の精度
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        //位置情報取得間隔(m)
         locationManager.distanceFilter = 300
-        
-        // 位置情報を取得
         locationManager.startUpdatingLocation()
     }
 
+    func updateIcon(coordinate: CLLocationCoordinate2D) {
+        let weatherAPI = WAPIManager(apiKey: "9eeaea80076ac9f4b5f19ec445d707f3", temperatureFormat: .Celsius, lang: .English)
+        weatherAPI.currentWeatherByCoordinatesAsJson(coordinates: coordinate) {
+            (WeatherResult) -> Void in
+            print(WeatherResult)
+        }
+    }
 }
 
 extension ViewController: CLLocationManagerDelegate {
 
-    /** 位置情報取得成功時 */
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            longitude = location.coordinate.longitude
-            latitude = location.coordinate.latitude
-            print(String(longitude))
-            print(String(latitude))
+            updateIcon(coordinate: location.coordinate)
         }
     }
 
-    /** 位置情報取得失敗時 */
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         NSLog("Error")
     }
