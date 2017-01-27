f9bd4ca4b9d1c8c6224698c6062fae638daafe6f
大石弘一郎 <k_ooishi@waku-2.com>
2017-01-27 14:51:17 +0900

    現在地取れるように

--- WeatherIcon/ViewController.swift
+++ WeatherIcon/ViewController.swift
@@ -7,25 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
 import SwiftOpenWeatherMapAPI
 
 class ViewController: UIViewController {
 
+    var locationManager: CLLocationManager! = nil
+    var longitude: CLLocationDegrees!
+    var latitude: CLLocationDegrees!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let weatherAPI = WAPIManager(apiKey: "9eeaea80076ac9f4b5f19ec445d707f3", temperatureFormat: .Celsius, lang: .English)
+//        let weatherAPI = WAPIManager(apiKey: "9eeaea80076ac9f4b5f19ec445d707f3", temperatureFormat: .Celsius, lang: .English)
+//
+//        weatherAPI.currentWeatherByCityNameAsJson(cityName: "London") {
+//            (WeatherResult) -> Void in
+//            print(WeatherResult)
+//        }
         
-        weatherAPI.currentWeatherByCityNameAsJson(cityName: "London") { (result) -> Void in
-            print(result)
-        }
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        //位置情報取得の可否。バックグラウンドで実行中の場合にもアプリが位置情報を利用することを許可する
+        locationManager.requestAlwaysAuthorization()
+        //位置情報の精度
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //位置情報取得間隔(m)
+        locationManager.distanceFilter = 300
+        
+        // 位置情報を取得
+        locationManager.startUpdatingLocation()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+
+    /** 位置情報取得成功時 */
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            longitude = location.coordinate.longitude
+            latitude = location.coordinate.latitude
+            print(String(longitude))
+            print(String(latitude))
+        }
+    }
+
+    /** 位置情報取得失敗時 */
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog("Error")
+    }
+
+}
